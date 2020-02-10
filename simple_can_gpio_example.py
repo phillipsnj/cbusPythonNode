455b3e4f93dd132b785fd49e4b268bafd510d594
phillipsnj <nigeljphillips@yahoo.co.uk>
2020-02-10 15:59:10 +0000

    Cleaned Code

---
+++ simple_can_gpio_example.py
@@ -0,0 +1,56 @@
+import cbus_node
+import json
+from gpiozero import LED, Button
+
+node_id = 252  # ID number for the new node.
+
+redled = LED(22)
+amberled = LED(23)
+greenled = LED(24)
+button = Button(17)
+
+
+def main_func(msg):  # Define the function to perform processing.
+    # print("MESSAGE : "+json.dumps(msg, indent=4))
+    if msg["task"] == "on":
+        # msg["variables"]["LED"].on()
+        if "redled" in msg["variables"]["LED"]:
+            redled.on()
+        if "amberled" in msg["variables"]["LED"]:
+            amberled.on()
+        if "greenled" in msg["variables"]["LED"]:
+            greenled.on()
+    else:
+        # msg["variables"]["LED"].off()
+        if "redled" in msg["variables"]["LED"]:
+            redled.off()
+        if "amberled" in msg["variables"]["LED"]:
+            amberled.off()
+        if "greenled" in msg["variables"]["LED"]:
+            greenled.off()
+
+
+node = cbus_node.CanNode(node_id, main_func)  # Create the cbus node
+
+
+def button_on():
+    # redled.on()
+    node.ason(2)
+    # return 0
+
+
+def button_off():
+    # redled.off()
+    node.asof(2)
+    # return 0
+
+
+button.when_pressed = button_on
+button.when_released = button_off
+
+node.teach_long_event(257, 4, {"id": 1, "LED": ["redled"]})  # Teach a long event (nodeId, eventId, parameters)
+node.teach_long_event(257, 32, {"id": 1, "LED": ["greenled", "amberled"]})
+
+# node.run()
+node.start()
+node.join()
